d34ce86ec258b5e723d44c3364fb874f3f43023a
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-03-08 15:59:51 +0530

    commit'

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
-
-import Input from "./components/input.js"
+import React,{useState} from 'react';
+// import Input from "./components/input.js"
 function App() {
+   const [name,setName]=useState("Dipesh");
+   const [Flag,setFlag] = useState(false);
+  const [Steps,setStep] = useState(0);
+  const [names,setNames] = useState([]);
+  function changeName(){
+    return setFlag("Nikesh");
+  }
+  function increment(){
+  return setStep(Steps+1);
+  }
+  function decrement(){
+    return setStep(Steps-1);
+  }
+  function addnames(e){
+        e.preventDefault();
+        setNames([...names,{id:names.length,name}]);
+        setNames("");
+  }
   return (
     <>
-    <h1>TO DO</h1>
-      <Input />
+     <div className='tbody'>
+     <h1>hello, {Flag ? name :"nope"}</h1>
+     <button onClick={changeName}>Click me</button>
+     <hr></hr>
+     <button onClick={increment}>+</button>
+     <div>{Steps}</div>
+     <button onClick={decrement}>-</button>
+     <hr></hr>
+<form onSubmit={addnames}>
+  <input type='text' value={name} placeholder="add names.." onChange={(e)=> setName(e.target.value)} />
+  <button >Submit</button>
+  
+</form>
+<ul>
+    {names.map((item)=>(
+      <li key={item.id}>{item.name}</li>
+    ))}
+    
+  </ul>
+     </div>
     </>
   );
 }
